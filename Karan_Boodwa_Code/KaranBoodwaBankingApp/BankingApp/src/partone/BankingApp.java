307bdd0973f8ee0208868d48b73f1c50c1c302e2
Karan Boodwa <karan@USF-Guest-Wifi-51-118.laptops.usf.edu>
2018-12-06 10:55:26 -0500

    Added starter files for Part 1 of the Banking App

---
+++ Karan_Boodwa_Code/KaranBoodwaBankingApp/BankingApp/src/partone/BankingApp.java
@@ -0,0 +1,17 @@
+package partone;
+
+/**
+ * Main driver class for the Banking Application.
+ * Handles initialization and initial I/O as well as connections between Users and Accounts
+ * @author Karan
+ *
+ */
+public class BankingApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the First Bank of Karan!");
+		// ...
+
+	}
+
+}
