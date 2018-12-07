2dea62413d2749c215717a8bde9ba22c6cd2f602
Chris West <west@107-144-156-227.biz.bhn.net>
2018-12-07 12:29:02 -0500

    Add

--- WestHWK1/src/eightteen/hwk/WordConversions.java
+++
@@ -1,8 +0,0 @@
-package eightteen.hwk;
-
-public abstract class WordConversions {
-	public abstract Boolean isUpperCase(String words);
-	public abstract String toUpperCase(String words);
-	public abstract int toInteger(String words);
-	public abstract void toString(String words);
-}
--- WestHWK1/src/fifteen/hwk/Mathematical.java
+++
@@ -1,8 +0,0 @@
-package fifteen.hwk;
-
-interface Mathematical {
-	double addition(double t, double s);
-	double subtraction(double t, double s);
-	double multiplication(double t, double s);
-	double division(double t, double s);
-}
