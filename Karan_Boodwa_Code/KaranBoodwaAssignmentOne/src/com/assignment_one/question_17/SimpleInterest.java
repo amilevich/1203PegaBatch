5ba0d436f785eedab90b0201cceccfe080ff8e91
Karan Boodwa <karan@USF-Gold-Wifi-226-106-62.laptops.usf.edu>
2018-12-07 10:00:27 -0500

    q17 updated to use try-with-resources

--- Karan_Boodwa_Code/KaranBoodwaAssignmentOne/src/com/assignment_one/question_17/SimpleInterest.java
+++ Karan_Boodwa_Code/KaranBoodwaAssignmentOne/src/com/assignment_one/question_17/SimpleInterest.java
@@ -18,17 +18,27 @@ public class SimpleInterest {
 	}
 
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		System.out.println("Simple Interest Calculator!");
-		System.out.print("Enter principal: ");
-		double principal = s.nextDouble();
-		System.out.print("Enter rate (%): ");
-		double rate = s.nextDouble();
-		System.out.print("Enter number of years: ");
-		int time = s.nextInt();
 
-		System.out.println("$" + principal + " at " + rate + "%" + " after " + time + " years = $"
-				+ interest(principal, rate, time) + " interest");
+		/*
+		 * try-with-resources used with scanner as the user's input can't be guaranteed
+		 * to be numbers, causing an exception to be thrown Generally speaking unchecked
+		 * exceptions should not be caught but in this case it makes sense to catch
+		 * rather than code around
+		 */
+		try (Scanner s = new Scanner(System.in)) {
+			System.out.println("Simple Interest Calculator!");
+			System.out.print("Enter principal: ");
+			double principal = s.nextDouble();
+			System.out.print("Enter rate (%): ");
+			double rate = s.nextDouble();
+			System.out.print("Enter number of years: ");
+			int time = s.nextInt();
+			System.out.println("$" + principal + " at " + rate + "%" + " after " + time + " years = $"
+					+ interest(principal, rate, time) + " interest");
+
+		} catch (Exception e) {
+			System.err.println("Invalid Input. Please enter numbers.");
+		}
 
 	}
 
