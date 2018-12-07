b83699bd2afa2b49de4675f58932fe8153c7c4b0
Ihab Baghdadi <ihabbaghdadi@icloud.com>
2018-12-07 16:22:18 -0500

    Q10 HW Assignment

---
+++ Ihab_Baghdadi_Code/Q10.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw;
+
+/**
+ *
+ * @author ijb87
+ */
+public class Q10 {
+    public static void main(String args[])  
+{  
+    // variable declaration  
+    int n1 = 5, n2 = 10, max;  
+      
+    // Largest among n1 and n2  
+    max = (n1 > n2) ? n1 : n2;  
+      
+    // Print the largest number  
+    System.out.println("Largest number between " + n1 +  
+                  " and " + n2 + " is " + max + ". " );  
+} 
+}
