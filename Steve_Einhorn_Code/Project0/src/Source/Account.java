60e0c556b1a4b40f0b5652d2ddfb925aa98c2e14
Steve Einhorn <seinhorn24@gmail.com>
2018-12-10 20:20:58 -0500

    add Project0

---
+++ Steve_Einhorn_Code/Project0/src/Source/Account.java
@@ -0,0 +1,28 @@
+package Source;
+
+public class Account {
+
+	String accountNbr;
+	String userName;
+	double accountBal;
+	
+	public String getAccountNbr() {
+		return accountNbr;
+	}
+	public void setAccountNbr(String accountNbr) {
+		this.accountNbr = accountNbr;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public double getAccountBal() {
+		return accountBal;
+	}
+	public void setAccountBal(double accountBal) {
+		this.accountBal = accountBal;
+	}
+	
+}
---
+++ Steve_Einhorn_Code/Project0/src/Source/OpenApplication.java
@@ -0,0 +1,29 @@
+package Source;
+
+public class OpenApplication {
+
+	String username;
+	String password;
+	double initDeposit;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Double getInitDeposit() {
+		return initDeposit;
+	}
+	public void setInitDeposit(Double initDeposit) {
+		this.initDeposit = initDeposit;
+	}
+	
+
+}
