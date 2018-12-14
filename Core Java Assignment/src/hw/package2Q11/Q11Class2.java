9a016d2be280d9062cf65c74bfb381e58eb26393
Ihab Baghdadi <ihabbaghdadi@icloud.com>
2018-12-14 12:09:07 -0500

    Add Core Java Assignment

---
+++ Core Java Assignment/src/hw/package2Q11/Q11Class2.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw.package2Q11;
+import hw.package1Q11.*;
+/**
+ *
+ * @author ijb87
+ */
+public class Q11Class2 {
+    public float variable = 0;
+    public float variable2 = 1;
+}
