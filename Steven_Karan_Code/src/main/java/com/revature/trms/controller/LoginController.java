fc03f60ab3eb7be5c6566002ddce2f428991221e
Steven Lopez <steven.lopez@upr.edu>
2018-12-28 14:54:06 -0500

    add fixed typos in login

--- Steven_Karan_Code/src/main/java/com/revature/trms/controller/LoginController.java
+++ Steven_Karan_Code/src/main/java/com/revature/trms/controller/LoginController.java
@@ -8,28 +8,28 @@ import com.revature.trms.daoimpls.EmployeeDAOImpl;
 import com.revature.trms.models.Employee;
 
 public class LoginController {
-	
+
 	public static String Login(HttpServletRequest req) {
-		if(req.getMethod().equals("GET")) {
+		if (req.getMethod().equals("GET")) {
 			return "/hmtl/login.html";
 		}
 		String username = req.getParameter("username");
 		String password = req.getParameter("password");
-		
-		
+
 		EmployeeDAOImpl edi = new EmployeeDAOImpl();
 		Employee emp = new Employee();
-		
+
 		emp = edi.getEmployeeByUsername(username);
 		System.out.println("emp: " + emp);
 		System.out.println(BCrypt.checkpw(password, emp.getPassword()));
-		if(emp!=null)
-			if(username.equals(emp.getUsername()) && BCrypt.checkpw(password, emp.getPassword())) {
+		if (emp != null) {
+			if (username.equals(emp.getUsername()) && BCrypt.checkpw(password, emp.getPassword())) {
 				System.out.println("username" + username);
 				req.getSession().setAttribute("Employee", emp);
 				return "/html/index.html";
-			}	
-		
-		return "/hmtl/login.html";
+			}
+		}
+
+		return "/html/login.html";
 	}
 }
