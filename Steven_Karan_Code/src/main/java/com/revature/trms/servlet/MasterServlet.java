ce7435d69ca83e2dd4cd4c9a1d0c3c3ee7c6155d
Steven Lopez <steven.lopez@upr.edu>
2018-12-29 14:23:11 -0500

    Revert "Revert "added validation method to login :wrench:""
    This reverts commit 04e6492fe13f15612226ffd527425440fbbf7314.

--- Steven_Karan_Code/src/main/java/com/revature/trms/servlet/MasterServlet.java
+++ Steven_Karan_Code/src/main/java/com/revature/trms/servlet/MasterServlet.java
@@ -13,6 +13,7 @@ public class MasterServlet extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("hello from doGet request method");
 		String targetUrl = RequestHelper.process(req, resp);
 		req.getRequestDispatcher(targetUrl).forward(req, resp);
 	}
