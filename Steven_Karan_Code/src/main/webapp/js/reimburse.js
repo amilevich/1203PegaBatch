3911926815913ed1b0d2dd50810776a0d8d05fff
Karan Boodwa <kboodwa@gmail.com>
2019-01-05 17:35:44 -0500

    file upload button prettier

--- Steven_Karan_Code/src/main/webapp/js/reimburse.js
+++ Steven_Karan_Code/src/main/webapp/js/reimburse.js
@@ -3,3 +3,11 @@
 window.onload=function(){
  	getAlert();
  }
+
+ document.getElementById("attach-input").onchange = function () {
+ 	let file_arr = this.value.split("\\");
+ 	console.log(file_arr);
+ 	let filename = file_arr[file_arr.length-1];
+ 	console.log(filename);
+    document.getElementById("filename").innerHTML = filename;
+};
