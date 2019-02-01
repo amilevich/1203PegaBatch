8152f25b7b5c2f40ee29b7548a54d209deda178b
Jason Knighten <jmknighten9@gmail.com>
2019-02-01 10:00:35 -0500

    added Rest service pt 2

--- Rest/ZipCodeRestClient/src/main/java/com/revature/main/ClientDriver.java
+++ Rest/ZipCodeRestClient/src/main/java/com/revature/main/ClientDriver.java
@@ -2,6 +2,7 @@ package com.revature.main;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
@@ -24,6 +25,16 @@ public class ClientDriver {
 	ZipCodes z=getZipCodeResponse.readEntity(ZipCodes.class);
 	System.out.println(z);
 	System.out.println("************************************");
+	//Post Request
+			Builder postZipCodeBuilder= webTarget.request();
+			z= new ZipCodes(28532,"Cherry Point","North Carolina");
+			Response postZipCodeResponse=postZipCodeBuilder.accept(
+					MediaType.APPLICATION_JSON).post(Entity.entity(
+							z,MediaType.APPLICATION_JSON));
+			statusCode =postZipCodeResponse.getStatus();
+			System.out.println("Returned with status code: " +statusCode);
+			String s = postZipCodeResponse.readEntity(String.class);
+			System.out.println(s);
 	}
 
 }
